0b9671534beb98528d6ab6eef4d94ce29801bd7e
Edan <khaimovedan1901@gmail.com>
2024-05-04 23:12:10 +0300

    add test for list and array classes

---
+++ tests/TestDynamicArray.h
@@ -0,0 +1,87 @@
+#ifndef TEST_DYNAMIC_ARRAY_H
+#define TEST_DYNAMIC_ARRAY_H
+
+#include <cassert>
+#include "../DynamicArray.h"
+
+void testDynamicArrayConstructors()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+
+    DynamicArray<int> test;
+    assert(test.GetSize() == 0);
+
+    DynamicArray<int> test1(6);
+    assert(test1.GetSize() == 6);
+
+    DynamicArray<int> test2(arr,6);
+    assert(test2.GetSize() == 6);
+    for (int i = 0; i < test2.GetSize(); i++)
+    {
+        assert(test2.Get(i) == arr[i]);
+    }
+
+    DynamicArray<int> test3(test2);
+    assert(test2.GetSize() == test3.GetSize());
+    for (int i = 0; i < test2.GetSize(); i++)
+    {
+        assert(test2.Get(i) == test3.Get(i));
+    }
+}
+
+void testDynamicArrayGet()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    DynamicArray<int> test(arr,6);
+    for (int i = 0; i < test.GetSize(); i++)
+    {
+        assert(test.Get(i) == arr[i]);
+    }
+}
+
+void testDynamicArrayGetSize()
+{
+    const int n = 6;
+    int arr[n] = {1, 2, 3, 4, 5, 6};
+    DynamicArray<int> test(arr,n);
+    assert(test.GetSize() == n);
+}
+
+void testDynamicArraySet()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    DynamicArray<int> test(arr,6);
+    test.Set(0, 100);
+    assert(test.Get(0) == 100);
+}
+
+void testDynamicArrayResize()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    DynamicArray<int> test1(arr,6);
+    test1.Resize(8);
+    assert(test1.GetSize() == 8);
+    for (int i = 0; i < 6; i++)
+    {
+        assert(test1.Get(i) == arr[i]);
+    }
+    DynamicArray<int> test2(arr,6);
+    test2.Resize(2);
+    assert(test2.GetSize() == 2);
+    for (int i = 0; i < test2.GetSize(); i++)
+    {
+        assert(test1.Get(i) == arr[i]);
+    }
+}
+
+void testDynamicArrayIndexOperator()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    DynamicArray<int> test(arr,6);
+    for (int i = 0; i < test.GetSize(); i++)
+    {
+        assert(test[i] == arr[i]);
+    }
+}
+
+#endif
---
+++ tests/TestLinkedList.h
@@ -0,0 +1,142 @@
+#ifndef TEST_LINKED_LIST_H
+#define TEST_LINKED_LIST_H
+
+#include <cassert>
+#include "../LinkedList.h"
+
+void testLinkedListConstructors()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+
+    LinkedList<int> test;
+    assert(test.GetLength() == 0);
+
+    LinkedList<int> test1(arr, 6);
+    assert(test1.GetLength() == 6);
+    for (int i = 0; i < test1.GetLength(); i++)
+    {
+        assert(test1.Get(i) == arr[i]);
+    }
+
+    LinkedList<int> test2(test1);
+    assert(test1.GetLength() == test2.GetLength());
+    for (int i = 0; i < test1.GetLength(); i++)
+    {
+        assert(test1.Get(i) == test2.Get(i));
+    }
+}
+
+void testLinkedListGetFirst()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    assert(test.GetFirst() == arr[0]);
+}
+
+void testLinkedListGetLast()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    assert(test.GetLast() == arr[5]);
+}
+
+void testLinkedListGet()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    for (int i = 0; i < test.GetLength(); i++)
+    {
+        assert(test.Get(i) == arr[i]);
+    }
+}
+
+void testLinkedListGetSubList()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    LinkedList<int> *result = test.GetSubList(1, 3);
+    for (int i = 1; i < 3; i++)
+    {
+        assert(result->Get(i - 1) == test.Get(i));
+    }
+}
+
+void testLinkedListGetLength()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    assert(6 == test.GetLength());
+}
+
+void testLinkedListAppend()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    test.Append(129);
+    assert(test.GetLength() == 6 + 1);
+    for (int i = 0; i < test.GetLength() - 1; i++)
+    {
+        assert(test.Get(i) == arr[i]);
+    }
+    assert(test.Get(test.GetLength() - 1) == 129);
+}
+
+void testLinkedListPrepend()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    test.Prepend(129);
+    assert(test.GetLength() == 6 + 1);
+    for (int i = 1; i < test.GetLength(); i++)
+    {
+        assert(test.Get(i) == arr[i - 1]);
+    }
+    assert(test.Get(0) == 129);
+}
+
+void testLinkedListInsertAt()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    test.InsertAt(7, 2);
+    assert(test.GetLength() == 6 + 1);
+    for (int i = 0; i < 2; i++)
+    {
+        assert(test.Get(i) == arr[i]);
+    }
+    assert(test.Get(2) == 7);
+    for (int i = 3; i < test.GetLength(); i++)
+    {
+        assert(test.Get(i) == arr[i - 1]);
+    }
+}
+
+void testLinkedListConcat()
+{
+    int arr1[6] = {1, 2, 3, 4, 5, 6};
+    int arr2[3] = {213, 33, 2};
+    LinkedList<int> test1(arr1, 6);
+    LinkedList<int> test2(arr2, 3);
+    LinkedList<int> *res = test1.Concat(test2);
+    assert(res->GetLength() == test1.GetLength() + test2.GetLength());
+    for (int i = 0; i < test1.GetLength(); i++)
+    {
+        assert(res->Get(i) == test1.Get(i));
+    }
+    for (int i = 0; i < test2.GetLength(); i++)
+    {
+        assert(res->Get(test1.GetLength() + i) == test2.Get(i));
+    }
+}
+
+void testLinkedListIndexOperator()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    LinkedList<int> test(arr, 6);
+    for (int i = 0; i < test.GetLength(); i++)
+    {
+        assert(test[i] == arr[i]);
+    }
+}
+
+#endif
