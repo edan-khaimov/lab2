02cd5645b195cba9ec7f1baaee0225095cee91f0
Edan <khaimovedan1901@gmail.com>
2024-05-15 14:06:34 +0300

    fix of switch cases

--- main.cpp
+++ main.cpp
@@ -27,45 +27,49 @@ int main()
         switch (userChoice)
         {
             case 1:
-                switch (type)
+                if (type == 1)
                 {
-                    case 1:
-                        setSequence(listInt);
-                        sequenceInterface(listInt, userChoice);
-                        break;
-                    case 2:
-                        setSequence(listDouble);
-                        sequenceInterface(listDouble, userChoice);
-                        break;
-                    case 3:
-                        setSequence(listChar);
-                        sequenceInterface(listChar, userChoice);
-                        break;
-                    case 4:
-                        setSequence(listString);
-                        sequenceInterface(listString, userChoice);
-                        break;
+                    setSequence(listInt);
+                    sequenceInterface(listInt, userChoice);
                 }
+                else if (type == 2)
+                {
+                    setSequence(listDouble);
+                    sequenceInterface(listDouble, userChoice);
+                }
+                else if (type == 3)
+                {
+                    setSequence(listChar);
+                    sequenceInterface(listChar, userChoice);
+                }
+                else if (type == 4)
+                {
+                    setSequence(listString);
+                    sequenceInterface(listString, userChoice);
+                }
+                break;
             case 2:
-                switch (type)
+                if (type == 1)
                 {
-                    case 1:
-                        setSequence(arrayInt);
-                        sequenceInterface(arrayInt, userChoice);
-                        break;
-                    case 2:
-                        setSequence(arrayDouble);
-                        sequenceInterface(arrayDouble, userChoice);
-                        break;
-                    case 3:
-                        setSequence(arrayChar);
-                        sequenceInterface(arrayChar, userChoice);
-                        break;
-                    case 4:
-                        setSequence(arrayString);
-                        sequenceInterface(arrayString, userChoice);
-                        break;
+                    setSequence(arrayInt);
+                    sequenceInterface(arrayInt, userChoice);
                 }
+                else if (type == 2)
+                {
+                    setSequence(arrayDouble);
+                    sequenceInterface(arrayDouble, userChoice);
+                }
+                else if (type == 3)
+                {
+                    setSequence(arrayChar);
+                    sequenceInterface(arrayChar, userChoice);
+                }
+                else if (type == 4)
+                {
+                    setSequence(arrayString);
+                    sequenceInterface(arrayString, userChoice);
+                }
+                break;
         }
         userChoice = selectSequenceType();
         type = selectElementsType();
